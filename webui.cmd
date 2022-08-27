cc43c7b029fdb539ddf7c55f5e5d430b2bf55802
Danil Boldyrev <daswerq123@gmail.com>
2022-08-28 02:16:36 +0300

    sync with 131 commit

--- webui.cmd
+++ webui.cmd
@@ -1,8 +1,27 @@
 @echo off
-call C:\ProgramData\miniconda3\Scripts\activate.bat
+
+set paths=%ProgramData%\miniconda3\Scripts
+set paths=%paths%;%USERPROFILE%\miniconda3\Scripts
+set paths=%paths%;%ProgramData%\anaconda3\Scripts
+set paths=%paths%;%USERPROFILE%\anaconda3\Scripts
+
+for %%a in (%paths%) do ( 
+ if EXIST "%%a\activate.bat" (
+    SET CONDA_PATH=%%a
+ )
+)
+
+IF "%CONDA_PATH%"=="" (
+  echo anaconda3/miniconda3 not found. Install from here https://docs.conda.io/en/latest/miniconda.html
+  exit /b 1 
+) else (
+  echo anaconda3/miniconda3 detected in %CONDA_PATH%
+)
+
+call "%CONDA_PATH%\activate.bat"
 call conda env create -f environment.yaml
 call conda env update --file environment.yaml --prune
-call C:\ProgramData\miniconda3\Scripts\activate.bat ldo
+call "%CONDA_PATH%\activate.bat" ldo
 python "%CD%"\scripts\relauncher.py
 
 :PROMPT
@@ -12,3 +31,4 @@ IF EXIST "models\ldm\stable-diffusion-v1\model.ckpt" (
 ) ELSE (
   ECHO Your model file does not exist! Place it in 'models\ldm\stable-diffusion-v1' with the name 'model.ckpt'.
 )
+
